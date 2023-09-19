4ea83e296233b4103e11598b4bbd61e120bb0b3a
srmngp <sr.mngp@gmail.com>
2023-09-19 19:46:55 +0200

    ñapa con trycatch

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ import { rootRouter } from './src/routes/RootRouter.js'
 
 export const app = express()
 
-app.get('/', (req, res) => {
-  const path = '/item/123}'
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-})
+try {
+  app.disable('x-powered-by')
 
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params
-  res.end(`Item: ${slug}`)
-})
+  app.use('/', rootRouter)
+  app.use('/mix', mixRouter)
+
+  const PORT = process.env.PORT ?? 3000
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+  })
+
+} catch (error) {
+  console.log('que ta pachando')
+  console.log(error)
+}
