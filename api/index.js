102f69aebfd89adfef7f715de774c63229b028ef
srmngp <sr.mngp@gmail.com>
2023-09-19 16:08:10 +0200

    Another try

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
-import express from 'express'
-import { rootRouter } from '../src/routes/RootRouter'
-import { mixRouter } from '../src/routes/MixRouter'
+import { app } from '../index.js'
 
-export const app = express()
-
-app.disable('x-powered-by')
-
-app.use('/', rootRouter)
-app.use('/mix', mixRouter)
-
-const PORT = process.env.PORT ?? 3000
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+export default app
