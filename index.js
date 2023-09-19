1c4aeff758ec1532b34f2e919fe4763fcca9bff2
srmngp <sr.mngp@gmail.com>
2023-09-19 19:51:27 +0200

    Add error handling to index.js

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ import { rootRouter } from './src/routes/RootRouter.js'
 
 export const app = express()
 
-try {
-  app.disable('x-powered-by')
+app.disable('x-powered-by')
 
-  app.use('/', rootRouter)
-  app.use('/mix', mixRouter)
+app.use('/', rootRouter)
+app.use('/mix', mixRouter)
 
-  const PORT = process.env.PORT ?? 3000
-  app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`)
-  })
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(500).json({ message: 'Unexpected error', error: err.message })
+})
 
-} catch (error) {
-  console.log('que ta pachando')
-  console.log(error)
-}
+const PORT = process.env.PORT ?? 3000
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
+})
